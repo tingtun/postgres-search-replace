8e06d2cc50521d1ebc0b83bdd131b91724e9aa8a
Alexander Teinum <ateinum@gmail.com>
2012-09-04 13:08:58 +0200

    Add configurations_.py

---
+++ configurations_.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+configurations = {
+    '0': [
+        {
+            'host': 'localhost',
+            'port': 5432,
+            'user': '',
+            'password': '',
+            'database': '',
+            'columns': [
+                ('schema0', [
+                    ('table0', [
+                        'column0',
+                        'column1']),
+                    ('table1', [
+                        'column0',
+                        'column1']),
+                 ]),
+            ],
+        },
+#        {
+#            'host': 'localhost',
+#            'port': 5432,
+#            'user': '',
+#            'password': '',
+#            'database': '',
+#            'columns': [
+#                ('schema0', [
+#                    ('table0', [
+#                        'column0',
+#                        'column1']),
+#                    ('table1', [
+#                        'column0',
+#                        'column1']),
+#                ]),
+#            ],
+#        },
+    ],
+}
